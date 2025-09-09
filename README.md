83f1c0de015ed60ba7bad32797a4bcbbfb533ce4
Renan Cavichi <renancavichi@gmail.com>
2025-09-08 22:28:18 -0300

    TextInput

--- README.md
+++ README.md
@@ -2,12 +2,27 @@
 
 - O que é React Native
 - O que é Expo
-- Componente Básicos do React Native (View, Text, Stylesheet, Pressable, Image)
+- Componente Básicos do React Native (View, Text, Stylesheet, Pressable, Button, Image, TextInput)
 - Expo Image (Mais formatos de imagens além de recursos de placeholder)
 - Difereça entre ScrollView e Flatlist
-- Pasta app Expo Router (Navegação Baseada em Arquivos)
+- Pasta app Expo Router (Navegação Baseada em Arquivos) - React Navigation
 - Tipos de Navegação Stack, Tabs, Drawer e Modal
-    - useRouter (router.push, router.replace)
+    - useRouter (router.push, router.replace, router.navigate)
     - Link
 - Persistência de Dados com AsyncStorage (Async)
+    - Apenas String
     - setItem, getItem e removeItem
+- TextInput
+- Notificações e Publição de Apps
+
+### Notificações
+    - https://www.youtube.com/watch?v=BCCjGtKtBjE
+    
+    Notification Tool 
+    - https://expo.dev/notifications 
+
+### Build para Android
+    - https://www.youtube.com/watch?v=nxlt8uwqhpE
+
+### Build para iOS
+    - https://www.youtube.com/watch?v=VZL_e0cEwo8
--- src/app/signup.js
+++ src/app/signup.js
@@ -1,9 +1,49 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Platform} from "react-native";
+import { useState } from "react";
+import Button from "../components/ui/Button";
 
 export default function SignUp() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSignup = () => {
+        console.log(name, email, pass);
+        //fetch api
+        console.log(Platform.OS)
+
+    }
+
     return (
         <View style={styles.container}>
             <Text>SignUp</Text>
+
+            <Text>Nome:</Text>
+            <TextInput
+                style={[styles.input, {height: 70}]}
+                value={name}
+                onChangeText={setName}
+                multiline={true}
+            />
+            <Text>Email:</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="nome@email.com"
+                placeholderTextColor={'#999'}
+                value={email}
+                onChangeText={setEmail}
+            />
+             <Text>Pass:</Text>
+            <TextInput
+                style={styles.input}
+                //placeholder="Digite seu nome"
+                value={pass}
+                onChangeText={setPass}
+                secureTextEntry={true}
+            />
+
+            <Button title="Cadastrar" onPress={handleSignup} style={{marginTop: 20}} />
         </View>
     )
 }
@@ -13,5 +53,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    input:{
+        borderWidth: 1,
+        width: '80%',
+        borderColor: '#212121ff',
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 7
     }
 })
